6cbda512d79c2de07af7bb990a6d85123388fef4
Md Irfan <irfan@Mds-MacBook-Air.local>
2025-05-30 22:23:55 +0530

    chore: run locally on port

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/react-husky-eslint-prettier/', // <-- important for GitHub Pages
-})
+  base: mode === 'development' ? '/' : '/react-husky-eslint-prettier/', // dynamic base
+}))
